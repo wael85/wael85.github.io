a2e217606df8f5d2e2476c73f712cfdc7b0709c8
Wael Haded <49494281+wael85@users.noreply.github.com>
2023-04-09 02:29:38 +0200

    Add build folder

--- src/routes/Pkemons.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Main from "../Componentes/Main/Main";
-import Header from "../Componentes/Header/Header";
-import Footer from "../Componentes/Footer/Footer";
-
-export default function Pokemons() {
-  return <Main />;
-}
